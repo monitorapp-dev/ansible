7383e472c16eb8f2643774a263026057d8c736fc
HSSON <hsson@monitorapp.com>
2020-10-19 18:17:53 +0900

    Add module login

---
+++ collection/plugins/modules/login.py
@@ -0,0 +1,34 @@
+#!/bin/env python
+#-*- coding: utf-8 -*-
+
+from ansible.module_utils.basic import *
+import requests
+import json
+
+def Login(sIP, sID, sPassword):
+    sURL = 'https://' + sIP + ':223/v1/auth/login'
+    jsQuery = {"id": sID, "pwd": sPassword}
+    jsheaders = {"Content-Type": "application/json"}
+
+    response = requests.post(sURL, data=json.dumps(jsQuery), headers=jsheaders, verify=False)
+    #response.encoding = 'utf-8'
+    
+    return {"response": response.json()}
+
+if __name__ == '__main__':
+    module_args = dict(
+        ip=dict(type='str', required=True),
+        id=dict(type='str', required=True),
+        password=dict(type='str', required=True),
+    )
+    module = AnsibleModule(argument_spec=module_args, supports_check_mode=True)
+
+    jsResult = Login(module.params['ip'], module.params['id'], module.params['password'])
+
+    if 'error' in jsResult["response"]:
+        jsResult["status"] = -1
+        module.fail_json(msg=jsResult)
+    else:
+        jsResult["status"] = 0
+        module.exit_json(msg=jsResult)
+   
